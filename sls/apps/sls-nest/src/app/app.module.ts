bcd03ef497693cba5cd4e71356aebb67a131706e
Brian McIlwain <technicallyemployed@gmail.com>
2021-02-17 14:25:59 -0600

    Instantiate logger

--- sls/apps/sls-nest/src/app/app.module.ts
+++ sls/apps/sls-nest/src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
+import { environment } from '../environments/environment';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: environment.logLevel,
+        prettyPrint: environment.prettyPrintLogs,
+        useLevelLabels: false,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
