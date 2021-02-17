3c2c90f401545025b8faadbc305517c1aae73f5e
Brian McIlwain <technicallyemployed@gmail.com>
2021-02-17 13:43:24 -0600

    Removed project reference

--- sls/webpack.config.js
+++ sls/webpack.config.js
@@ -20,9 +20,6 @@ module.exports = {
         use: [
           {
             loader: 'ts-loader',
-            options: {
-              projectReferences: true
-            }
           },
         ],
       },
