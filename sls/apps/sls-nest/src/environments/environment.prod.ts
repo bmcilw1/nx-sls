7f830d86afe6246ce75bb3b2fe829c77f61867b4
Brian McIlwain <technicallyemployed@gmail.com>
2021-02-17 11:21:07 -0600

    Added handler file

--- sls/apps/sls-nest/src/environments/environment.prod.ts
+++ sls/apps/sls-nest/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true,
+  production: false,
+  origins: '*',
+  host: '0.0.0.0',
+  port: 3333,
+  prettyPrintLogs: false,
+  logLevel: 'debug',
 };
--- sls/apps/sls-nest/src/main.ts
+++ sls/apps/sls-nest/src/main.ts
@@ -1,21 +1,16 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import { environment } from './environments/environment';
+import { bootstrapServer, cachedServer } from './handler';
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function startServer() {
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  await bootstrapServer(globalPrefix);
+
+  const port = environment.port;
+  const host = environment.host;
+
+  cachedServer.listen(port, host, () => {
+    console.log(`Listening at ${host}:${port}/${globalPrefix}`);
   });
 }
 
-bootstrap();
+startServer();
