c8ce5f4716c85f6afa2b9fa06ca92c8c704aac66
Brian McIlwain <technicallyemployed@gmail.com>
2021-02-17 11:21:47 -0600

    Added webpack

---
+++ sls/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const slsWebpack = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  externals: [nodeExternals()],
+  devtool: 'source-map',
+  entry: slsWebpack.lib.entries,
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx'],
+    plugins: [new tsconfigPathsPlugin()],
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [],
+  mode: slsWebpack.lib.webpack.isLocal ? 'development' : 'production',
+  stats: 'minimal', // errors-only, minimal, none, normal, verbose
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+};
